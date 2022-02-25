068ae19c58117f961f332eb6f16c7cf98d579122
qiaogaojian <qiaogaojian@vip.qq.com>
2022-02-25 16:29:33 +0800

    feat: truffle 配置

---
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.4;
+
+contract Tether {
+    string public name = "Mock Tether Token";
+    string public symbol = "mUSDT";
+    uint256 public totalSupply = 1000000000000000000000000; // 1百万
+    uint8 public decimals = 18;
+}
