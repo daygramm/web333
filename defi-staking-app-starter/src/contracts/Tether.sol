bc0071e49da2d214674101f88a6024e15cd6fb7c
qiaogaojian <qiaogaojian@vip.qq.com>
2022-02-26 10:19:17 +0800

    feat: 添加转账方法

--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -5,4 +5,38 @@ contract Tether {
     string public symbol = "mUSDT";
     uint256 public totalSupply = 1000000000000000000000000; // 1百万
     uint8 public decimals = 18;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    error insufficientBalance(
+        uint requested,
+        uint avaliable
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(){
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint value) public returns (bool success) {
+        if(value > balanceOf[msg.sender]){
+            revert insufficientBalance(value,balanceOf[msg.sender]);
+            return false;
+        }
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender,to,value);
+        return true;
+    }
 }
