e30ab600df7f08445d8f8b8f56483f7bf8d34da3
qiaogaojian <qiaogaojian@vip.qq.com>
2022-02-26 09:48:11 +0800

    feat: 设置部署合约地址

--- defi-staking-app-starter/truffle-config.js
+++ defi-staking-app-starter/truffle-config.js
@@ -6,7 +6,8 @@ module.exports = {
         development: {
             host: "127.0.0.1",
             port: "7545",
-            network_id: "*"
+            network_id: "*",
+            from:"8882107d420fb7b4349c175090a3d52fb069ea854d9d661e70dc5d3a1757dfae"
         }
     },
     contracts_directory: "./src/contracts",
