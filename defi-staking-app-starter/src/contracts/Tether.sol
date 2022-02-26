a4f463d6a4f6e05a25ad814fca7de50b9b1a502e
qiaogaojian <qiaogaojian@vip.qq.com>
2022-02-26 17:33:33 +0800

    feat： 自定义token的转账和授权

--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -6,16 +6,12 @@ contract Tether {
     uint256 public totalSupply = 1000000000000000000000000; // 1百万
     uint8 public decimals = 18;
 
-    event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint _value
-    );
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-    event Approve(
+    event Approval(
         address indexed _owner,
         address indexed _spender,
-        uint _value
+        uint256 _value
     );
 
     //    error insufficientBalance(
@@ -24,12 +20,13 @@ contract Tether {
     //    );
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor(){
+    constructor() {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address to, uint value) public returns (bool success) {
+    function transfer(address to, uint256 value) public returns (bool success) {
         //        if (value > balanceOf[msg.sender]) {
         //            revert insufficientBalance(value, balanceOf[msg.sender]);
         //            return false;
@@ -38,6 +35,30 @@ contract Tether {
         balanceOf[msg.sender] -= value;
         balanceOf[to] += value;
         emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public returns (bool success) {
+        require(value <= balanceOf[from]);
+        require(value <= allowance[from][msg.sender]);
+
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
         return true;
     }
 }
