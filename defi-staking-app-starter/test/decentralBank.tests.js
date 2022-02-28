8e1e039119bf230f839975e65398edea8418c9f4
qiaogaojian <qiaogaojian@vip.qq.com>
2022-02-28 14:20:20 +0800

    feat: 测试前提条件

--- defi-staking-app-starter/test/decentralBank.tests.js
+++ defi-staking-app-starter/test/decentralBank.tests.js
@@ -1,4 +1,5 @@
 const { assert } = require("console");
+const { default: Web3 } = require("web3");
 
 const Tether = global.artifacts.require("Tether");
 const RWD = global.artifacts.require("RWD");
@@ -8,7 +9,24 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-describe("DecentralBank", (accounts) => {
+contract("DecentralBank", ([accounts]) => {
+  let tether;
+  let reward;
+  let decentralBank;
+
+  function tokens(number) {
+    return Web3.utils.toWei(number, "ether");
+  }
+
+  before(async () => {
+    tether = await Tether.new();
+    reward = await RWD.new();
+    decentralBank = await DecentralBank.new(reward.address, tether.address);
+
+    await reward.transfer(decentralBank.address,tokens("1000000"));
+    await tether.transfer(accounts[1],tokens("100"),{from:accounts[0]});
+  });
+
   describe("Mock Tether Deployment", async () => {
     it("matches name successfully", async () => {
       let tether = await Tether.new();
