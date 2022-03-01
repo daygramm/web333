4ad48cbdf486ef14b080254b519bd2578650b6aa
qiaogaojian <qiaogaojian@vip.qq.com>
2022-03-01 17:58:20 +0800

    feat: react布局的两种方式

---
+++ defi-staking-app-starter/src/components/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div className='text-center' style={
+                {
+                    color: 'deepskyblue'
+                }}>
+                <h className="content">Hello World!</h>
+            </div>
+        );
+    }
+}
+
+export default App;
