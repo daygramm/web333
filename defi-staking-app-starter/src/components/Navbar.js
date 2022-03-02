8166ff6d339738266f92481d70917c0a1ae03e0a
qiaogaojian <qiaogaojian@vip.qq.com>
2022-03-02 09:29:36 +0800

    feat: 布局和格式化

--- defi-staking-app-starter/src/components/Navbar.js
+++ defi-staking-app-starter/src/components/Navbar.js
@@ -4,22 +4,14 @@ import bank from "../bank.png";
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-dark fixed-top" style={{
-                background: "black",
-                height: 60
-            }}>
-                <a style={{
-                    color: "white"
-                }}>
-                    <img src={bank} width={50} height={30}/>
-
+            <nav className="navbar navbar-dark fixed-top" style={{background: "black", height: 60}}>
+                <a className="navbar-brand col-sm-3 col-md-2 mr-0" style={{color: "white"}}>
+                    <img src={bank} width={50} height={30} className="d-inline-block align-top"/>
                     DAPP Yield Staking (Decentralized Bank)
                 </a>
-                <ul>
-                    <li>
-                        <small style={{
-                            color: "white"
-                        }}>
+                <ul className="navbar-nav px-3">
+                    <li className="text-nowrap d-none nav-item d-sm-none d-sm-block">
+                        <small style={{color: "white"}}>
                             ACCOUNT NUMBER:
                         </small>
                     </li>
