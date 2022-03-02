3cab1b65fd0ce94b994043ddf213d2bd1b3e4921
qiaogaojian <qiaogaojian@vip.qq.com>
2022-03-02 18:30:09 +0800

    feat: 点击事件方法

--- defi-staking-app-starter/src/components/App.js
+++ defi-staking-app-starter/src/components/App.js
@@ -32,6 +32,8 @@ class App extends Component {
                         tetherBalance={this.state.tetherBalance}
                         rwdBalance={this.state.rwdBalance}
                         stakingBalance={this.state.stakingBalance}
+                        stakeTokens={this.stakeTokens}
+                        unstakeTokens={this.unstakeTokens}
                     />
         }
 
@@ -111,6 +113,22 @@ class App extends Component {
     async UNSAFE_componentWillMount() {
         await this.loadWeb3();
         await this.loadWeb3Data();
+    }
+
+    stakeTokens = (amount) => {
+        this.setState({loading: true});
+        this.state.tether.methods.approve(this.state.decentralBank.address, amount).send({from: this.state.account}).on("transactionHash", (hash) => {
+        });
+        this.state.decentralBank.methods.depositTokens(amount).send({from: this.state.account}).on("transactionHash", (hash) => {
+            this.setState({loading: false});
+        });
+    }
+
+    unstakeTokens = () => {
+        this.setState({loading: true});
+        this.state.decentralBank.methods.unstakeTokens().send({from: this.state.account}).on("transactionHash", (hash) => {
+            this.setState({loading: false});
+        });
     }
 }
 
