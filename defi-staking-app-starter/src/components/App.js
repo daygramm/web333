5e177d4cdbbc073fd51783edc84c31d7ec9360db
qiaogaojian <qiaogaojian@vip.qq.com>
2022-03-02 10:04:23 +0800

    feat: 连接MetaMask

--- defi-staking-app-starter/src/components/App.js
+++ defi-staking-app-starter/src/components/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
 import "./App.css";
 import Navbar from "./Navbar"
+import Web3 from "web3";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            account: "0x0"
+            account: "0x0123456"
         }
     }
 
@@ -15,10 +16,32 @@ class App extends Component {
             <div>
                 <Navbar account={this.state.account}/>
                 <div className='text-center' style={{color: 'deepskyblue'}}>
-                    <h className="content">Hello World!</h>
+                    {/*<h className="content">Hello World!</h>*/}
                 </div>
             </div>
         );
+    }
+
+    async loadWeb3() {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            await window.ethereum.enable();
+        } else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider);
+        } else {
+            alert("No ethereum browser detected! You can check out MetaMask!");
+        }
+    }
+
+    async loadWeb3Data() {
+        const Web3 = window.web3;
+        const account = await Web3.eth.getAccounts();
+        console.log(account);
+    }
+
+    async UNSAFE_componentWillMount() {
+        await this.loadWeb3();
+        await this.loadWeb3Data();
     }
 }
 
