224ebde3393d4f9781a45ed42af317d0a8daa44a
qiaogaojian <qiaogaojian@vip.qq.com>
2022-03-03 15:14:08 +0800

    feat: 调整样式

--- defi-staking-app-starter/src/components/Main.js
+++ defi-staking-app-starter/src/components/Main.js
@@ -10,13 +10,13 @@ class Main extends Component {
             <div id='content' className='mt-3'>
                 <table className='table text-muted text-center'>
                     <thead>
-                    <tr style={{color: 'black'}}>
+                    <tr style={{color: 'white'}}>
                         <th scope='col'>Staking Balance</th>
                         <th scope='col'>Reward Balance</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr style={{color: 'black'}}>
+                    <tr style={{color: 'white'}}>
                         <td>{window.web3.utils.fromWei(this.props.stakingBalance, "Ether")} USDT</td>
                         <td>{window.web3.utils.fromWei(this.props.rwdBalance, "Ether")} RWD</td>
                     </tr>
