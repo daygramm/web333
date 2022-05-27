e0d95b328450079708df285d379a364cef572e6a
qiaogaojian <qiaogaojian@vip.qq.com>
2022-05-27 15:42:19 +0800

    eslint

--- Truffle/truffle-tutorial/test/TruffleTutorial.js
+++ Truffle/truffle-tutorial/test/TruffleTutorial.js
@@ -1,4 +1,4 @@
-const {assert} = require("chai")
+const { assert } = require("chai")
 
 const TruffleTutorial = artifacts.require("./TruffleTutorial.sol")
 
@@ -6,42 +6,50 @@ require("chai")
     .use(require("chai-as-promised"))
     .should();
 
-contract('TruffleTutorial', ([contractOwner, secondAddress, thirdAddress]) => {
-        let truffleTutorial
+contract('TruffleTutorial', ([contractOwner, secondAddress, thirdAddress]) =>
+{
+    let truffleTutorial
 
-        before(async () => {
-            truffleTutorial = await TruffleTutorial.deployed()
+    before(async () =>
+    {
+        truffleTutorial = await TruffleTutorial.deployed()
+    })
+
+    describe("deployment", () =>
+    {
+        it("deploys successfully", async () =>
+        {
+            const address = await truffleTutorial.address
+
+            assert.notEqual(address, "")
+            assert.notEqual(address, undefined)
+            assert.notEqual(address, null)
+            assert.notEqual(address, 0x0)
         })
 
-        describe("deployment", () => {
-            it("deploys successfully", async () => {
-                const address = await truffleTutorial.address
+        it("has a message", async () =>
+        {
+            const message = await truffleTutorial.message()
 
-                assert.notEqual(address, "")
-                assert.notEqual(address, undefined)
-                assert.notEqual(address, null)
-                assert.notEqual(address, 0x0)
-            })
+            assert.equal(message, "Hello World!")
+        })
+    })
 
-            it("has a message", async () => {
-                const message = await truffleTutorial.message()
+    describe("message", () =>
+    {
+        it("contract owner sets a message", async () =>
+        {
+            await truffleTutorial.setMessage("Hi there!", { from: contractOwner })
+            const message = await truffleTutorial.message()
 
-                assert.equal(message, "Hello World!")
-            })
+            assert.equal(message, "Hi there!")
         })
 
-        describe("message", () => {
-            it("contract owner sets a message", async () => {
-                await truffleTutorial.setMessage("Hi there!", {from: contractOwner})
-                const message = await truffleTutorial.message()
-
-                assert.equal(message, "Hi there!")
-            })
-
-            it("address that is not the owner fails to set a message", async () => {
-                await truffleTutorial.setMessage("Hi there!", {from: secondAddress}).should.be.rejected
-                await truffleTutorial.setMessage("Hi there!", {from: thirdAddress}).should.be.rejected
-            })
+        it("address that is not the owner fails to set a message", async () =>
+        {
+            await truffleTutorial.setMessage("Hi there!", { from: secondAddress }).should.be.rejected
+            await truffleTutorial.setMessage("Hi there!", { from: thirdAddress }).should.be.rejected
         })
-    }
+    })
+}
 )
