9155e23cd91b614f5b78e167428320c49399542f
qiaogaojian <qiaogaojian@vip.qq.com>
2022-05-28 17:30:33 +0800

    测试hardhat转账

--- Hardhat/hardhat-tutorial/test/Token.js
+++ Hardhat/hardhat-tutorial/test/Token.js
@@ -13,6 +13,21 @@ describe("Token contract", function ()
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
     });
 }
+);
 
+describe("Transactions", function ()
+{
+    it("Should transfer tokens between accounts", async function ()
+    {
+        const [owner, addr1, addr2] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("Token");
+        const hardhatToken = await Token.deploy();
 
+        await hardhatToken.transfer(addr1.address, 50);
+        expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+
+        await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+        expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+    });
+}
 );
