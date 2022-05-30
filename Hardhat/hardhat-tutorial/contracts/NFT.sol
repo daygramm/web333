3756b71bfb8cec7a47f630bb18eafbc85281b88e
qiaogaojian <qiaogaojian@vip.qq.com>
2022-05-30 11:32:28 +0800

    NFT合约

---
+++ Hardhat/hardhat-tutorial/contracts/NFT.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity >=0.7.0 <=0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NFT", "NFT") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
--- Hardhat/hardhat-tutorial/scripts/deploy.js
+++ Hardhat/hardhat-tutorial/scripts/deploy.js
@@ -2,15 +2,19 @@ const { ethers } = require("hardhat");
 
 async function main ()
 {
+    // Contract Token
     const [deployer] = await ethers.getSigners();
-
     console.log("Deploying contracts with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
     const Token = await ethers.getContractFactory("Token");
     const token = await Token.deploy();
+    console.log("Contract Token deployed to address:",token.address);
 
-    console.log("Token address:", token.address);
+    // Contract NFT
+    const NFT = await ethers.getContractFactory("NFT");
+    const nft = await NFT.deploy();
+    console.log("Contract NFT deployed to address:",nft.address);
 }
 
 main()
