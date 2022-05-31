593efae0e23696e3d7226f1c60d25d73eb241ad7
qiaogaojian <qiaogaojian@vip.qq.com>
2022-05-31 16:29:19 +0800

    配置

--- Web3py/Tools/dotenv_test.py
+++ Web3py/Tools/dotenv_test.py
@@ -1,11 +1,20 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv("../config/.env")
 
+print(os.getenv("MODE"))
 if os.getenv("MODE") == "dev":
-    print("开发模式")
-elif os.getenv("MODE") == "test":
-    print("测试模式")
+    print("开发环境")
+    load_dotenv("../config/.env.dev")
+    print(os.getenv("NAME"))
+
+elif os.getenv("MODE") == "prod":
+    print("生产环境")
+    load_dotenv("../config/.env.prod")
+    print(os.getenv("NAME"))
+    
 else:
-    print("生产模式")
+    print("本地环境")
+    load_dotenv("../config/.env.local")
+    print(os.getenv("NAME"))
