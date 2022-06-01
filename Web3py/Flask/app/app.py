04ee7bc5ff6b585b9783216314e5b2e243df8c8e
qiaogaojian <qiaogaojian@vip.qq.com>
2022-06-01 10:40:47 +0800

    flask 主页模板数据

--- Web3py/Flask/app/app.py
+++ Web3py/Flask/app/app.py
@@ -3,6 +3,20 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
