4b3bea07837b7076a68842a9d922a9541baa8cb7
qiaogaojian <qiaogaojian@vip.qq.com>
2022-06-07 09:21:24 +0800

    redis 测试

--- Web3py/DB/redis_test.py
+++ Web3py/DB/redis_test.py
@@ -6,16 +6,24 @@ redis = redis_client.RedisClient(db=2, env=_evn)
 
 if __name__ == "__main__":
 
-    # 列表操作
-    redis.master.rpush("test_list", 1, 2, 3)
+    # 字符串
+    # redis.master.set("test_string_key","test_string_value")
 
-    list_len = redis.slave.llen("test_list")
-    print("列表长度:{}".format(list_len))
-
-    list_test = redis.slave.lrange("test_list",0,-1)
-    print("列表:{}".format(list_test))
+    # string_value = redis.slave.get("test_string_key")
+    # print(string_value)
     
-    redis.master.delete("test_list")
+    # redis.master.delete("test_string_key")
+
+    # 列表操作
+    # redis.master.rpush("test_list", 1, 2, 3)
+
+    # list_len = redis.slave.llen("test_list")
+    # print("列表长度:{}".format(list_len))
+
+    # list_test = redis.slave.lrange("test_list",0,-1)
+    # print("列表:{}".format(list_test))
+    
+    # redis.master.delete("test_list")
 
     # 散列表
     # redis.master.hset("test_hash_table", "key", "value")
