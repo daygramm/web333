f48149182a42aeb9916929e79b77c36871a3984c
qiaogaojian <qiaogaojian@vip.qq.com>
2022-06-08 15:24:53 +0800

    获取pending和supply测试版

--- Web3py/Web3/getPendingCount.py
+++ Web3py/Web3/getPendingCount.py
@@ -10,10 +10,10 @@ import redis_client
 import requests
 
 _evn = "test"
-redis = redis_client.RedisClient(db=2, env=_evn)
+redis = redis_client.RedisClient(db=0, env=_evn)
 
 db = pymysql.connect(
-    host="coder.53site.com", user="admin", password="Mangosteen0!", database="metabus"
+    host="47.114.151.253", user="admin", password="Mangosteen0!", database="metabus"
 )
 # db = pymysql.connect(
 #     host="172.21.157.7", user="magister_jvm", password="Mangosteen0!", database="metabus"
@@ -22,6 +22,9 @@ cursor = db.cursor()
 web3_url = "https://mainnet.infura.io/v3/23e4a77870ea4deab047bb6911a28144"
 web3_wss = Web3( Web3.HTTPProvider(web3_url))
 web3 = Web3(Web3.HTTPProvider(web3_url))
+erc721_abi = json.loads(
+    '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ApprovalCallerNotOwnerNorApproved","type":"error"},{"inputs":[],"name":"ApprovalQueryForNonexistentToken","type":"error"},{"inputs":[],"name":"ApprovalToCurrentOwner","type":"error"},{"inputs":[],"name":"ApproveToCaller","type":"error"},{"inputs":[],"name":"BalanceQueryForZeroAddress","type":"error"},{"inputs":[],"name":"MintToZeroAddress","type":"error"},{"inputs":[],"name":"MintZeroQuantity","type":"error"},{"inputs":[],"name":"OwnerQueryForNonexistentToken","type":"error"},{"inputs":[],"name":"TransferCallerNotOwnerNorApproved","type":"error"},{"inputs":[],"name":"TransferFromIncorrectOwner","type":"error"},{"inputs":[],"name":"TransferToNonERC721ReceiverImplementer","type":"error"},{"inputs":[],"name":"TransferToZeroAddress","type":"error"},{"inputs":[],"name":"URIQueryForNonexistentToken","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"flipSale","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxFreePerWallet","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxPerTx","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"count","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"mintEnabled","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"price","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"uri","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"setFreeAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newPrice","type":"uint256"}],"name":"setPrice","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalFree","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
+)
 
 app = Flask(__name__, instance_path="/{project_folder_abs_path}/instance")
 contracts = []
@@ -38,12 +41,59 @@ def get_all_contracts():
     return contracts
 
 
+def send_dingtalk(text):
+    headers = {'Content-Type': 'application/json; charset=UTF-8'}
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=0e1bcb4ca5583283e0070ac47a0ec9da62fba2aadd7edecf9e5f593fb77c2854'    
+    data = {
+        "msgtype": "markdown",
+        "markdown": {
+            "title": "NFT数量监控",
+            "text": text
+        }
+    }
+    print(requests.post(url, headers=headers, data=json.dumps(data)))
+
+def get_supply(contracts):
+    i = 0
+    l = len(contracts)
+    logT = []
+    logS = []
+    for c in contracts:
+        address = Web3.toChecksumAddress(c)
+        contract = web3.eth.contract(address=address, abi=erc721_abi)
+        
+        try:                    
+            totalSupply = contract.functions.totalSupply().call()
+            key = str(c).lower()+'_total_supply'
+            redis.master.set(key, totalSupply)
+            print("{}/{}.totalSupply: {}".format(i,l,redis.slave.get(key)))
+        except Exception as e:
+            # print("合约: {} 找不到 totolSupply 对应的函数, Exception: {}".format(c,e))
+            logT.append("- 合约: {} 找不到 totolSupply 对应的函数, Exception: {}\n".format(c,e))
+
+        try:
+            maxSupply = contract.functions.maxSupply().call()
+            key = str(c).lower()+'_max_supply'
+            redis.master.set(key, maxSupply)
+            print("{}/{}.maxSupply: {}".format(i,l,redis.slave.get(key)))
+        except Exception as e:
+            # print("合约: {} 找不到 maxSupply 对应的函数, Exception: {}".format(c,e))
+            logS.append("- 合约: {} 找不到 maxSupply 对应的函数, Exception: {}\n".format(c,e))
+
+        i = i + 1
+    msgT = "### totalSupply:\n{}".format(''.join(logT)) 
+    msgS = "### maxSupply:\n{}".format(''.join(logS))
+    print(msgT)
+    print(msgS)
+    # send_dingtalk(msgT + "\n" + msgS)
+
 # 每小时更新一次合约地址
 async def update_contracts():
     global contracts
     while True:
         contracts = get_all_contracts()
-        print("update contract: {}".format(str(contracts)))
+        # print("update contract: {}".format(str(contracts)))
+        get_supply(contracts)
         await asyncio.sleep(3600)
 
 
@@ -99,7 +149,7 @@ async def check_txs_status():
                     except Exception as e:
                         pass
                 
-
+            # get_supply(contracts)
         except Exception as e:
             print(e)
         await asyncio.sleep(3)
@@ -133,6 +183,6 @@ if __name__ == "__main__":
 
     # pending
     loop = asyncio.get_event_loop()
-    tasks = [check_txs_status(), subscribe_pending()]
+    tasks = [check_txs_status(), subscribe_pending(), update_contracts()]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
