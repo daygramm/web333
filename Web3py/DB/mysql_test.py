861ea2881c8dbdb9753f251d00680077902fe649
qiaogaojian <qiaogaojian@vip.qq.com>
2022-07-08 07:26:58 +0800

    本地数据库测试

--- Web3py/DB/mysql_test.py
+++ Web3py/DB/mysql_test.py
@@ -1,15 +1,18 @@
-import MySQLdb
+import pymysql
 
 # 打开数据库连接
-db = MySQLdb.connect(
-    "coder.53site.com", "admin", "Mangosteen0!", "metabus", charset="utf8"
+db = pymysql.connect(
+    host="192.168.1.172",
+    user="root",
+    password="love1115",
+    database="mysql"
 )
 
 # 使用cursor()方法获取操作游标
 cursor = db.cursor()
 
 # 创建数据表SQL语句
-sql = """SELECT * FROM free_mint_catched"""
+sql = """SELECT * FROM user"""
 cursor.execute(sql)
 
 results = cursor.fetchall()
